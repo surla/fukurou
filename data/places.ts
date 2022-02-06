a9611a6b0245249da162cc5d3acc3aa59fa027f2
Joseph Surla <josephsurla@gmail.com>
2022-02-06 15:41:19 -0800

    add places data

---
+++ data/places.ts
@@ -0,0 +1,36 @@
+export const places = [
+  {
+    id: "1",
+    imageUrl: "",
+    title: "Kochia Hill",
+    location: "Hitachinaka, Japan",
+    description: "See the beautiful sea of crimson red kochia bushes.",
+    category: "Nature",
+  },
+  {
+    id: "2",
+    imageUrl: "",
+    title: "Kawachi Wisteria Tunnel",
+    location: "Kitayushu, Japan",
+    description: "Walk through a bautiful tunnel of cascading violet flowers.",
+    category: "Nature",
+  },
+  {
+    id: "3",
+    imageUrl: "",
+    title: "Giant Gundam Robot",
+    location: "Tokyo, Japan",
+    description:
+      "A giant white statue of a cult sci-fi novel & anime character.",
+    category: "Attraction",
+  },
+  {
+    id: "4",
+    imageUrl: "",
+    title: "Ghibli Museum",
+    location: "Tokyo, Japan",
+    description:
+      "Explore the whimsical museum dedicated to the famed animation studio.",
+    category: "Museum",
+  },
+];
