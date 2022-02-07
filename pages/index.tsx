12527dd26a8406138b93619ccd1e4d532ce0cdc2
Joseph Surla <josephsurla@gmail.com>
2022-02-06 18:53:20 -0800

    install prisma and create schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { places } from "../data/places";
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,19 @@ const Home: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <h1 className="text-center text-3xl font-bold">fukurou.</h1>
+      <div className="container mx-auto my-20 max-w-5xl">
+        <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+          {places.map((place) => (
+            <li key={place.id} className="max-w-md  rounded  shadow">
+              <div className="flex flex-col space-y-2 p-5">
+                <p className="text-sm text-blue-500">{place.category}</p>
+                <p className="text-lg font-medium">{place.title}</p>
+                <p className="text-gray-600">{place.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
